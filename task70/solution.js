d689f5c3c99d1be8083ee0992aca3927c160a562
vv_tomilin <backatom1@gmail.com>
2021-04-21 11:45:32 +0500

    task_70

---
+++ task70/solution.js
@@ -0,0 +1,15 @@
+/**
+ * Fix the printDailyAffirmation function.
+ *
+ * It should print the string "I will become a JS developer in ** current year **!"
+ *
+ * Make sure to substitute the `** current year **` section with the actual year and remove the quotes.
+ *
+ * Example output: I will become a JS developer in 2325!
+ *
+ * P.S. Make the minimum amount of changes to the program!
+ * */
+
+export const printDailyAffirmation = () => console.log(`I will become a JS developer in 2021!`);
+
+printDailyAffirmation();
