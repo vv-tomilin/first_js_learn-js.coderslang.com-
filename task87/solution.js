1167d21d8bf8043cb2960900bf6b2bd665c88be0
vv_tomilin <backatom1@gmail.com>
2021-04-24 11:52:15 +0500

    task_87

---
+++ task87/solution.js
@@ -0,0 +1,11 @@
+/**
+ * Create 5 constants.
+ *
+ * Choose any names you like.
+ * */
+
+const isMyConst = true;
+const otherVar = 1;
+const variable = 2;
+const myVar = "Hello Var!";
+const floatVar = 1.5;
