66042666023db957eb3130838d43317fc1df29ab
vv_tomilin <backatom1@gmail.com>
2021-04-24 11:52:53 +0500

    task_88

---
+++ task90/solution.js
@@ -0,0 +1,9 @@
+/**
+ * Import constants from the constants.js file and log them to the console using console.log
+ *
+ * Each console.log should log a separate constant to the screen
+ * */
+import {MAX_THREADS, API_URL} from "./constants.js";
+
+console.log(MAX_THREADS);
+console.log(API_URL);
