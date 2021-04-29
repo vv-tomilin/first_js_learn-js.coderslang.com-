c9d45b212876d623b193b16b4d7e8433ea6e8bde
vv_tomilin <backatom1@gmail.com>
2021-04-29 12:04:54 +0500

    task_111

---
+++ task111/helper.js
@@ -0,0 +1,12 @@
+export const user = {
+  name: 'John',
+  surename: 'Golt',
+  age: '100',
+  address: {
+    country: 'US',
+    state:'NC',
+    city: 'Chikago',
+    postcode: '12458',
+    street: '456 Green Rd.'
+  }
+};
