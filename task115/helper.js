c35b7c9ef401f621cc2acbef3eafb06974129383
vv_tomilin <backatom1@gmail.com>
2021-05-13 12:10:25 +0500

    task_115

---
+++ task115/helper.js
@@ -0,0 +1,18 @@
+const animal = {
+	name: "Bobik",
+	type: "Dog",
+}
+
+const human = {
+	sex: "male",
+	name: "Linda",
+	age: "25",
+}
+
+const myObject = {
+	parameterOne: 1,
+	parameterTwo: 2,
+	parameterThree: 3,
+}
+
+export const objects = [animal, human, myObject];
